df04962a9fb739ed9568d00c145626fa31cb7a09
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-08-19 17:20:38 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
   end
 
   get '/friends' do
-    @friends = ["Jerry", "Barry", "Larry", "Mary", "Gary", "Karrie", "Clair E."]
+    @friends = ["Jerry", "Perry", "Terry", "Barry", "Harry", "Larry", "Mary", "Gary", "Karrie", "Clair E.", "Blair E."]
     erb :friends
 
   end
